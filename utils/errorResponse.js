8536a010f16ea2ff21973785d2d5973b0e7dbe4f
shawrani714-max <shawrani714@gmail.com>
2025-09-25 15:08:29 +0530

    Update errorResponse.js

--- utils/errorResponse.js
+++ utils/errorResponse.js
@@ -1,32 +1,13 @@
-const Wishlist = require('../models/Wishlist');
-const Order = require('../models/Order');
-const User = require('../models/User');
-const emailService = require('./emailService');
-
-async function notifyRestock(book) {
-  // Find users with this book in their wishlist
-  const wishlists = await Wishlist.find({ 'items.book': book._id }).populate('user');
-  const wishlistUsers = wishlists.map(w => w.user);
-
-  // Find users who ordered this book and were refunded
-  const refundedOrders = await Order.find({
-    'items.book': book._id,
-    paymentStatus: 'refunded'
-  }).populate('user');
-  const refundedUsers = refundedOrders.map(o => o.user);
-
-  // Merge and deduplicate users
-  const allUsers = [...wishlistUsers, ...refundedUsers].filter(Boolean);
-  const uniqueUsers = Array.from(new Set(allUsers.map(u => u._id.toString()))).map(id => allUsers.find(u => u._id.toString() === id));
-
-  // Send email to each user
-  for (const user of uniqueUsers) {
-    await emailService.sendMail({
-      to: user.email,
-      subject: `Book Available: ${book.title}`,
-      html: `<p>Good news! The book <strong>${book.title}</strong> is now back in stock. <a href="/book/${book._id}">View Book</a></p>`
-    });
+class ErrorResponse extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
   }
 }
 
-module.exports = notifyRestock;
+// Async error handler wrapper
+const catchAsync = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
+
+module.exports = { ErrorResponse, catchAsync }; 
